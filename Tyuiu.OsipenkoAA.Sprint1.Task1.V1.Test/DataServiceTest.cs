97216fc8b759f04ab9ec6f634f70c53f573c9ce1
Anastasia Osipenko <324472738+Anastasia845@users.noreply.github.com>
2026-09-23 15:14:24 +0500

    Add files via upload

---
+++ Tyuiu.OsipenkoAA.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.OsipenkoAA.Sprint1.Task1.V1.Lib;
+
+namespace Tyuiu.OsipenkoAA.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExperession()
+        {
+            DataService ds = new DataService();
+            double a = 1.0;
+            double x = 2.0;
+            double y = 3.0;
+            var res = ds.Calculate(a, x, y);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
---
+++ Tyuiu.OsipenkoAA.Sprint1.Task1.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.OsipenkoAA.Sprint1.Task1.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.OsipenkoAA.Sprint1.Task1.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0868c8a0-0f8a-40a3-9ec6-926d5978ee32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
